438bfc4ddc5ac4787ccf347483d5d8cdc350ba91
TannerOliver <435.tanner@gmail.com>
2021-10-23 15:13:39 -0500

    Creating database and seeds file to work out of

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,29 @@
 -- create schema for building database here
+DROP DATABASE IF EXISTS team_db;
+CREATE DATABASE team_db;
+
+USE team_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES  department(id)
+    ON DELETE SET NULL
+);
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
